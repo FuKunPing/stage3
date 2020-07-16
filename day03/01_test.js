e0afb5ccf5deba5d9676c9e61ec4875b97fd46b1
alpha <123@qq.com>
2020-07-16 11:58:09 +0800

    ads

---
+++ day03/01_test.js
@@ -0,0 +1,11 @@
+console.log("使用NodeJs运行的js");
+
+function show(){
+    for(var i=0;i<5;i++){
+        console.log(i);
+    }
+}
+show();
+// window.alert(1);
+// console.log(location);
+console.log(global);
